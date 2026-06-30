0fdc170d3d454e58681692f8d1f49e7055b05d16
vruppuluri <venkat.uppuluri@gmail.com>
2026-06-30 12:25:58 -0700

    Add files via upload

--- lessons/day26/scripts/dev.sh
+++ lessons/day26/scripts/dev.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 RESOURCE_GROUP_NAME=terraform-state-rg
-STAGE_SA_ACCOUNT=tfstagebackend2024piyush
-DEV_SA_ACCOUNT=tfdevbackend2024piyush
+STAGE_SA_ACCOUNT=tfstagebackend2024vru
+DEV_SA_ACCOUNT=tfdevbackend2024vru
 CONTAINER_NAME=tfstate
 
 
 # Create resource group
-az group create --name $RESOURCE_GROUP_NAME --location canadacentral
+az group create --name $RESOURCE_GROUP_NAME --location westus2
 
 # Create storage account for staging environment
 az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STAGE_SA_ACCOUNT --sku Standard_LRS --encryption-services blob
